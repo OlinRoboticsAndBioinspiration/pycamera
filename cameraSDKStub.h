98bf7033d90973dfad8b9436dcd8767f52d8aca0
Aaron Hoover <amhoov@gmail.com>
2013-07-15 14:10:46 -0400

    Deleted stub cpp and h files

--- cameraSDKStub.h
+++
@@ -1,9 +0,0 @@
-#include "cameralibrary.h"
-#include <windows.h>
-#include <stdio.h>
-#include <tchar.h>
-
-bool WaitForInitialization();
-void Shutdown();
-
-//void setFrameRate(int serial, int rate);
